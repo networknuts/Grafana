20bc277d617518003c976e49c2890a0c9ad531b7
arytmw <54942957+arytmw@users.noreply.github.com>
2023-07-22 13:31:01 +0530

    Create grafana-install.sh

---
+++ grafana-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get install -y apt-transport-https
+apt-get install -y software-properties-common wget
+wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] \
+              https://packages.grafana.com/enterprise/deb stable main" | sudo \
+              tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install grafana-enterprise
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl enable grafana-server

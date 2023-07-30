5d0b5ab9da65b01d4a18ca57f39e5237655bd275
arytmw <54942957+arytmw@users.noreply.github.com>
2023-07-30 11:54:37 +0530

    Create deploy-blackbox.sh

---
+++ deploy-blackbox.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.24.0/blackbox_exporter-0.24.0.linux-amd64.tar.gz
+tar -xvf blackbox_exporter-0.24.0.linux-amd64.tar.gz
+sudo useradd -rs /bin/false blackbox
+sudo cp blackbox_exporter-0.24.0.linux-amd64/blackbox_exporter /usr/bin/blackbox_exporter
+sudo chmod 777 /usr/bin/blackbox_exporter
+sudo cp blackbox_exporter-0.24.0.linux-amd64/blackbox.yml /usr/bin/blackbox.yml
+sudo chmod 444 /usr/bin/blackbox.yml
+sudo cp blackbox.service /etc/systemd/system/blackbox.service
+sudo systemctl daemon-reload
+sudo systemctl start blackbox
+sudo systemctl enable blackbox

19b0f2a9e636a1604961aadb57f289145ea3b81b
arytmw <54942957+arytmw@users.noreply.github.com>
2025-03-10 16:03:34 +0530

    Update prometheus-install.sh

--- prometheus-install.sh
+++ prometheus-install.sh
@@ -9,15 +9,15 @@ sudo useradd --no-create-home --shell /bin/false prome
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 # Downloading and extracting files
-wget https://github.com/prometheus/prometheus/releases/download/v2.45.0/prometheus-2.45.0.linux-amd64.tar.gz
-tar -xvf prometheus-2.45.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/prometheus/releases/download/v3.2.1/prometheus-3.2.1.linux-amd64.tar.gz
+tar -xvf prometheus-3.2.1.linux-amd64.tar.gz
 # Copying files to respective locations and changing permissions
-sudo cp prometheus-2.45.0.linux-amd64/prometheus /usr/local/bin/
-sudo cp prometheus-2.45.0.linux-amd64/promtool /usr/local/bin/
+sudo cp prometheus-3.2.1.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-3.2.1.linux-amd64/promtool /usr/local/bin/
 sudo chown prome:prome /usr/local/bin/prometheus
 sudo chown prome:prome /usr/local/bin/promtool
-sudo cp -r prometheus-2.45.0.linux-amd64/consoles /etc/prometheus
-sudo cp -r prometheus-2.45.0.linux-amd64/console_libraries /etc/prometheus
+sudo cp -r prometheus-3.2.1.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-3.2.1.linux-amd64/console_libraries /etc/prometheus
 sudo chown -R prome:prome /etc/prometheus/consoles
 sudo chown -R prome:prome /etc/prometheus/console_libraries
 sudo chown -R prome:prome /var/lib/prometheus/

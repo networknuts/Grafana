6187d85a6b8b1713cb07ea56f9d1735141f4c2f2
arytmw <54942957+arytmw@users.noreply.github.com>
2025-03-11 15:23:25 +0530

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.9.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.0/node_exporter-1.9.0.linux-amd64.tar.gz
 tar -xvf node_exporter-1.9.0.linux-amd64.tar.gz
 sudo cp node_exporter-1.9.0.linux-amd64/node_exporter /usr/local/bin
 sudo useradd --no-create-home --shell /bin/false node_exporter
